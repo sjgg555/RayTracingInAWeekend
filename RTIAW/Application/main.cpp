dad765ffe20bdf44eb591993243ba640d36a614d
sjgg555 <sjgg555@gmail.com>
2022-03-22 21:21:04 +0000

    6.2 Cleaning up

--- RTIAW/Application/main.cpp
+++ RTIAW/Application/main.cpp
@@ -3,24 +3,36 @@
 
 #include <iostream>
 
-bool hit_sphere(const point3& centre, float radius, const Ray& r) 
+float hit_sphere(const point3& centre, float radius, const Ray& r) 
 {
 	vec3 oc = r.origin() - centre;
-	auto a = dot(r.direction(), r.direction());
-	auto b = 2.0f * dot(oc, r.direction());
-	auto c = dot(oc, oc) - radius * radius;
-	auto discriminant = (b * b) - (4 * a * c);
-	return (discriminant > 0);
+	auto a = glm::length2(r.direction());
+	auto half_b = dot(oc, r.direction());
+	auto c = glm::length2(oc) - radius * radius;
+
+	auto discriminant = half_b * half_b - a * c;
+	if (discriminant < 0)
+	{
+		return -1.0f;
+	}
+	else
+	{
+		return (-half_b - sqrt(discriminant)) / a;
+	}
 }
 
 color ray_color(const Ray& r)
 {
-	if (hit_sphere(point3(0,0,-1), 0.5, r))
+	//check for sphere hit
+	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+	if (t > 0.0f)
 	{
-		return color(1, 0, 0);
+		vec3 N = normalize(r.at(t) - vec3(0, 0, -1));
+		return 0.5f * color(N.x + 1, N.y + 1, N.z + 1);
 	}
+	//return the background otherwise
 	vec3 unit_direction(glm::normalize(r.direction()));
-	float t = 0.5f * (unit_direction.y + 1.0);
+	t = 0.5f * (unit_direction.y + 1.0);
 	return (1.0f - t) * color(1.0f, 1.0f, 1.0f) + (t * color(0.5f, 0.7f, 1.0f));
 }
 
--- RTIAW/Application/stdafx.h
+++ RTIAW/Application/stdafx.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <glm/glm.hpp>
+#include <glm/gtx/norm.hpp>
 using namespace glm;
 
 using point3 = vec3;
